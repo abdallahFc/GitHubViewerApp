53cb6ee6fd8ba071f6267edf8333b315af1ca158
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-08-23 21:36:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/githubviewerapp/GithubApplication.kt
@@ -0,0 +1,8 @@
+package com.example.githubviewerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GithubApplication :Application() {
+}
---
+++ app/src/main/java/com/example/githubviewerapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.githubviewerapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.githubviewerapp.ui.feature.home.HomeScreen
+import com.example.githubviewerapp.ui.theme.GitHubViewerAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GitHubViewerAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
